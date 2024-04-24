e98907f15540a2810fb9804f5bb8cc48242af9d9
mmmtobezip <mmmszip@gmail.com>
2024-04-24 17:18:17 +0900

    feat Prob4

--- practice01/src/main/java/prob4/Prob4.java
+++ practice01/src/main/java/prob4/Prob4.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class Prob4 {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.print("문자열을 입력하세요 : ");
-		String text = scanner.nextLine();
-
-		
-		scanner.close();
-	}
-
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.print("문자열을 입력하세요.: ");
+    String str = sc.nextLine();
+    
+    for (int i = 0; i < str.length(); i++) {
+      System.out.println(str.substring(0, i));
+    }
+  }
 }
