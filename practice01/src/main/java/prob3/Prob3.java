da496b2f89d28d6027f41210e1dfb231cabf996d
mmmtobezip <mmmszip@gmail.com>
2024-04-24 17:18:06 +0900

    feat Prob3

--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -3,13 +3,33 @@ package prob3;
 import java.util.Scanner;
 
 public class Prob3 {
-	
-	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-		/* 코드 작성 */
-		
-		scanner.close();
-	}
+    System.out.print("숫자를 입력하세요: ");
+    int num = sc.nextInt();
+
+    if (num < 0) {
+      System.out.println("음수는 입력할 수 없습니다");
+    } else {
+      int sum = calculateSum(num);
+      System.out.println("결과값: " + sum);
+    }
+    sc.close();
+  }
+
+  public static int calculateSum(int num) {
+    int sum = 0;
+    if(num % 2 == 1) {
+      for (int i = 1; i <= num; i += 2) {
+        sum += i;
+      }
+    } else {
+      for (int i = 2; i <= num; i += 2) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
 }
