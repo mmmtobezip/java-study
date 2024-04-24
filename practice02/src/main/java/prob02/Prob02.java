24a083344c1b4a69f0d98c7b33bfda86a7e3cabd
mmmtobezip <mmmszip@gmail.com>
2024-04-24 17:19:00 +0900

    feat practice/Prob02

--- practice02/src/main/java/prob02/Prob02.java
+++ practice02/src/main/java/prob02/Prob02.java
@@ -11,10 +11,18 @@ public class Prob02 {
 		double sum = 0;
 
 		/* 키보드에서 배열 크기만큼 입력 받아 배열에 저장하는 코드 */
+		System.out.println("5개의 숫자를 입력하세요.");
+		for(int i = 0; i < intArray.length; i++) {
+			intArray[i] = scanner.nextInt();
+		}
 		
 		/* 배열에 저장된 정수 값 더하기 */
-		
+		for(int i = 0; i < intArray.length; i++) {
+			sum += intArray[i];
+		}
 		/* 출력 */
+		double result = sum / intArray.length;
+		System.out.printf("평균은 %.1f 입니다", result); //printf()
 		
 		/* 자원정리 */
 		scanner.close();
