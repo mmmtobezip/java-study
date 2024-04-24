711c6556b41d62ce9d641dbf4d639a2ee39466df
mmmtobezip <mmmszip@gmail.com>
2024-04-24 17:17:36 +0900

    feat Prob01

--- practice01/src/main/java/prob1/Prob1.java
+++ practice01/src/main/java/prob1/Prob1.java
@@ -3,11 +3,17 @@ package prob1;
 import java.util.Scanner;
 
 public class Prob1 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in );
-		
-		/* 코드 작성 */
-		
-		scanner.close();
-	}
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.print("수를 입력하시오: ");
+    int num = sc.nextInt();
+
+    if(num%3==0) {
+      System.out.println("3의 배수입니다.");
+    } else {
+      System.out.println("3의 배수가 아닙니다.");
+    }
+    sc.close();
+  }
 }
