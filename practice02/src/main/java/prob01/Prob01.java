3da023df8b7b1afaae63ef16d0d19ce03764a3a1
mmmtobezip <mmmszip@gmail.com>
2024-04-24 17:18:49 +0900

    feat practice02/Prob01

--- practice02/src/main/java/prob01/Prob01.java
+++ practice02/src/main/java/prob01/Prob01.java
@@ -5,11 +5,21 @@ import java.util.Scanner;
 public class Prob01 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in  );
-
-		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
-
-		/* 코드 작성 */
+	
+		System.out.print("금액: ");
+		int input = scanner.nextInt();
 		
+		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
+		String[] result = {"50000원", "10000원", "5000원", "1000원", "500원", "100원", "50원", "10원", "5원", "1원"};
+		
+		for(int i = 0; i < MONEYS.length; i++) {
+			int cnt = input / MONEYS[i];
+			
+			if(cnt > 0) {
+				System.out.println(result[i] + ": " + cnt + "개");
+				input %= MONEYS[i];
+			}
+		}
 		scanner.close();
  	}
 }
