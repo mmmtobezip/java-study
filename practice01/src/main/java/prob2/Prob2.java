3bc853ae9f9fa8d19c2473668580ef3c3e03c4c2
mmmtobezip <mmmszip@gmail.com>
2024-04-24 17:17:57 +0900

    feat Prob2

--- practice01/src/main/java/prob2/Prob2.java
+++ practice01/src/main/java/prob2/Prob2.java
@@ -2,6 +2,13 @@ package prob2;
 
 public class Prob2 {
 	public static void main(String[] args) {
-		/* 코드 작성 */
+		int n = 10;
+
+	    for(int i = 1; i<=n; i++) {
+	      for(int j = i; j<=i+n-1; j++) {
+	        System.out.print(j+ " ");
+	      }
+	      System.out.println();
+	    }
 	}
 }
