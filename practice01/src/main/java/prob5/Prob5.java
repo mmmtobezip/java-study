76effb63ab83cca9b8b22f07890b6d2bb42d11f2
mmmtobezip <mmmszip@gmail.com>
2024-04-24 17:18:30 +0900

    feat Prob5

--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -2,6 +2,25 @@ package prob5;
 
 public class Prob5 {
 
-	public static void main(String[] args) {
-	}
+  public static void main(String[] args) {
+    for (int i = 1; i <= 99; i++) {
+      String num = String.valueOf(i);
+      int cnt = 0;
+
+      for (int j = 0; j < num.length(); j++) {
+        char c = num.charAt(j);
+        if (c == '3' || c == '6' || c == '9') {
+        	cnt++;
+        }
+      }
+
+      if (cnt > 0) {
+        System.out.print(i+ " ");
+        for (int j = 0; j < cnt; j++) {
+          System.out.print("짝");
+        }
+        System.out.println();
+      }
+    }
+  }
 }
