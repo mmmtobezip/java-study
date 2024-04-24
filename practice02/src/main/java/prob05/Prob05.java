3bab1f25dc0a75d154413f3df2a34d6559523fae
mmmtobezip <mmmszip@gmail.com>
2024-04-25 08:46:10 +0900

    feat practice02/Prob05

--- practice02/src/main/java/prob05/Prob05.java
+++ practice02/src/main/java/prob05/Prob05.java
@@ -5,28 +5,44 @@ import java.util.Scanner;
 
 public class Prob05 {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in );
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner( System.in );
 
-		while( true ) {
-			
-			/* 게임 작성 */
+    while( true ) {
+      // 정답 램덤하게 만들기
+      Random random = new Random();
+      int correctNumber = random.nextInt( 100 ) + 1;
 
-			// 정답 램덤하게 만들기
-			Random random = new Random();
-			int correctNumber = random.nextInt( 100 ) + 1;
-			System.out.println(correctNumber);
+      System.out.println("수를 결정하였습니다. 맞추어 보세요.");
 
-			
-			//새 게임 여부 확인하기
-			System.out.print( "다시 하겠습니까(y/n)>>" );
-			String answer = scanner.next();
-			if( "y".equals( answer ) == false ) {
-				break;
-			}
-		}
-		
-		scanner.close();
-	}
+      int min = 1, max = 100;
+      int tryCount = 1;
+      int guessNumber;
 
+      do {
+        System.out.println(min + "-" + max);
+        System.out.print(tryCount + ">>");
+        guessNumber = scanner.nextInt();
+
+        if (guessNumber < correctNumber) {
+          System.out.println("더 높게");
+          min = guessNumber + 1;
+        } else if (guessNumber > correctNumber) {
+          System.out.println("더 낮게");
+          max = guessNumber - 1;
+        }
+
+        tryCount++;
+      } while (guessNumber != correctNumber);
+
+      System.out.println("맞혔습니다.");
+      //새 게임 여부 확인하기
+      System.out.print( "다시 하겠습니까(y/n)>>" );
+      String answer = scanner.next();
+      if( "y".equals( answer ) == false ) {
+        break;
+      }
+    }
+    scanner.close();
+  }
 }
