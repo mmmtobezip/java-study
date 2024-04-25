52e1b38548da8b61374c5be32879b3b4fc9346b1
mmmtobezip <mmmszip@gmail.com>
2024-04-25 16:09:30 +0900

    reset to 2d4399f

--- practice03/src/main/java/paint/Shape.java
+++
@@ -1,8 +0,0 @@
-package paint;
-
-public abstract class Shape implements Drawable{ //draw()메서드 추상클래스
-	Point[] points; 
-	String fillColor;
-	String lineColor;
-//	public abstract void draw(); 인터페이스로 구현
-}
