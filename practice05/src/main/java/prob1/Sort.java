48fddc64035258b00d57a51f0280b379ce8470df
mmmtobezip <mmmszip@gmail.com>
2024-04-29 02:13:28 +0900

    feat practice05/Prob01

--- practice05/src/main/java/prob1/Sort.java
+++ practice05/src/main/java/prob1/Sort.java
@@ -13,11 +13,18 @@ public class Sort {
 			System.out.print( array[ i ] + " " );
 		}
 		
-		//
-		// 정렬 알고리즘이 적용된 코드를 여기에 작성합니다.
-		//
-
-		
+		// 버블 정렬
+        for (int i = 0; i < count - 1; i++) {
+            for (int j = 0; j < count - i - 1; j++) {
+                // 내림차순 정렬
+                if (array[j] < array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        
 		// 결과 출력
 		System.out.println( "\nAfter Sort." );
 		
