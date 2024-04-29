100f032dd7b78f0aa3e59b4cf9e7633f0731bd33
mmmtobezip <mmmszip@gmail.com>
2024-04-29 14:07:10 +0900

    04. Java API 유틸리티 예제

---
+++ chapter04/src/main/java/chapter04/CalendarTest.java
@@ -0,0 +1,19 @@
+package chapter04;
+
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class CalendarTest {
+	public static void main(String[] args) {
+		Calendar.getInstance();
+		
+		// 1. Locale
+		Locale aLocale = Locale.getDefault(Locale.Category.FORMAT); //ca 캘린더 타입에 따라 
+		//System.out.println(aLocale); //ko_KR 출력
+		
+		//2. TimeZone : defaultTimeZone(aLocale);
+		TimeZone tz = TimeZone.getDefault();
+		//System.out.println(aLocale + ":" + tz);
+	}
+}
