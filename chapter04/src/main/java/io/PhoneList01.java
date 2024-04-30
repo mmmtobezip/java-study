e8489c3eadbc9adcca11ac5b20670fcd915cfb1d
mmmtobezip <mmmszip@gmail.com>
2024-04-30 14:32:24 +0900

    feat io with StringTokenizer

--- chapter04/src/main/java/io/PhoneList01.java
+++ chapter04/src/main/java/io/PhoneList01.java
@@ -1,5 +1,6 @@
 package io;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -8,9 +9,12 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
+import java.util.StringTokenizer;
 
 public class PhoneList01 {
 	public static void main(String[] args) {
+		BufferedReader br = null;
+		
 		try {
 			File file = new File("./phone.txt"); //File은 IO클래스가 아님. File객체를 FileInputStream에 넣어도됨.
 			if(!file.exists()) {
@@ -21,9 +25,6 @@ public class PhoneList01 {
 			System.out.println("=== 파일정보 ===");
 			System.out.println(file.getAbsolutePath()); //파일의 절대 위치
 			System.out.println(file.length() + "Bytes"); //byte단위로 나오는 파일의 길이
-			//System.out.println(file.lastModified()); //ms - long타입 
-			
-			//Date d = new Date(file.lastModified()); //Calendar도 가능
 			System.out.println(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date(file.lastModified())));
 			
 			
@@ -32,19 +33,47 @@ public class PhoneList01 {
 			//1. 기반스트림
 			FileInputStream fis = new FileInputStream(file); // new FileInputStream("./phone.txt")
 			
-			//2. 보조스트림(byte|byte|byte => char로 변환)
+			//2. 보조스트림1(byte|byte|byte => char로 변환)
 			InputStreamReader isr = new InputStreamReader(fis, "utf-8"); //(주스트림, 인코딩 유형)
-			fis.read(); //IOException 처리 필요
-			 
+			
+			//3. 보조스트림2(char|char|char|\n -> "charcharchar")
+			br = new BufferedReader(isr);
+			
+			//4. 처리
+			String line = null;
+			while((line = br.readLine()) != null) {
+				StringTokenizer st = new StringTokenizer(line, "\t "); //String들을 tab+space로 나눌 예정
+				
+				int index = 0;
+				while(st.hasMoreElements()) { //토큰이 더 남았는지 
+					String token = st.nextToken();
+					
+					//데이터 형식대로 뽑기 위함
+					if(index == 0) { //이름 
+						System.out.print(token + ":");
+					} else if(index == 1) { //전화번호1
+						System.out.print(token + "-");
+					} else if(index == 2) { //전화번호2
+						System.out.print(token + "-");
+					} else { //전화번호3
+						System.out.print(token + "\n");
+					}
+					index++;
+				}
+			}
 		} catch(UnsupportedEncodingException e) {
 			System.out.println("error:" + e);
 			
-		} catch (FileNotFoundException e) {
-			//catch안에선 log -> 사과 -> 자원해제(close)
-			//FileNotFoundException은 메모리를 많이 차지함. -> 로직을 추가하지 않기		
-		} catch (IOException e) { //FileNotFoundException은 IOException의 자식, 먼저 IOException으로 예외를 발생시켜도 됨. 
-								  //그렇기 때문에 IOException을 먼저 처리하면, FileNotFoundException을 적을 필요x (하위 예외로 들어갈 일이x)
-			
+		} catch(IOException e) {
+			System.out.println("error: " + e);
+		} finally {
+			try {
+				if(br != null) {
+					br.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
