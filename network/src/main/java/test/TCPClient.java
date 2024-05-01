154409f88355f2c0c3f563fad370d90dc695fb81
mmmtobezip <mmmszip@gmail.com>
2024-05-01 15:52:26 +0900

    add EchoClient 과제 구현 팁

--- network/src/main/java/test/TCPClient.java
+++ network/src/main/java/test/TCPClient.java
@@ -37,7 +37,7 @@ public class TCPClient {
 			}
 			
 			//38라인으로 온건 데이터를 잘 읽은 것 -> 인코딩 해준 후 화면에 뿌리기 
-			data = new String(buffer, 0, readByteCount, "UTF-8");
+			data = new String(buffer, 0, readByteCount, "UTF-8"); //Line40 -> 보조스트림인 InputStreamReader에게 맡기면 된다. (경계가 없는걸 "\n개행"을 붙여 보낸다.) 읽을 땐 개행 제외 bufferedReader로 읽기 
 			System.out.println("[Client] received:" + data);
 		} catch (SocketException e) {
 			System.out.println("[Client] suddenly closed by Server");
